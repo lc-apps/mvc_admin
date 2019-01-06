a723471357d4cecb256296d592c4df3f3a727cb6
Carlos Henrique <carlos_hpc@hotmail.com>
2019-01-05 23:19:47 -0200

    aula 1

--- app/controllers/admin/AdminController.php
+++ app/controllers/admin/AdminController.php
@@ -2,14 +2,16 @@
 
 namespace app\controllers\admin;
 
+use app\classes\Password;
+
 class AdminController extends \app\controllers\ContainerController {
 
 
 
 	public function index() {
 
-		return toJson(['name' => 'Carlos']);
 
+		return toJson(Password::hash('123'));
 		
 		$this->view([
 			'title' => 'Login',
